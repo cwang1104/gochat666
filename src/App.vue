078cc1967e0a111a1a7cd84d06f88b047ea9d95d
wangli <wangli@zhiannet.com>
2023-12-14 21:02:24 +0800

    feat: 页面搭建

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <router-view></router-view>
+  <Home></Home>
+
+  <!-- <router-view></router-view> -->
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Home from '@/components/home/Home.vue'
+</script>
 <style scoped lang="scss"></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,10 @@
 //注册全局组件
 import SvgIcon from '@/components/Svgicon.vue'
-const allGlobalComponent = {
+const allGlobalComponent: Record<string, any> = {
   SvgIcon,
 }
 export default {
-  install(app) {
-    console.log('install')
-
-    console.log(app)
-
+  install(app: any) {
     Object.keys(allGlobalComponent).forEach((key) => {
       app.component(key, allGlobalComponent[key])
     })
