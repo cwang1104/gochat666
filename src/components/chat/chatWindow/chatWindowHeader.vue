0640a8544254251a5b366e62e598d103dee300ce
wangli <wangli@zhiannet.com>
2023-12-22 18:15:21 +0800

    feat: 聊天框

---
+++ src/components/chat/chatWindow/chatWindowHeader.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="headerContainer">
+    <div class="userInfo">
+      <div class="avatar">
+        <img src="../../../assets/images/background.jpg" alt="" />
+      </div>
+      <div class="personInfo">
+        <span>name</span>
+        <span>title</span>
+      </div>
+    </div>
+    <div class="chatTools">
+      <el-icon color="white" size="25" class="svgItem"><VideoCamera /></el-icon>
+      <el-icon color="white" size="25" class="svgItem"><Tickets /></el-icon>
+      <el-icon color="white" size="25" class="svgItem"><Picture /></el-icon>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { VideoCamera, Tickets, Picture } from '@element-plus/icons-vue'
+</script>
+
+<style lang="scss" scoped>
+.headerContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px;
+
+  .userInfo {
+    display: flex;
+    // position: relative;
+    // top: 10px;
+    align-items: center;
+    .avatar {
+      position: relative;
+      width: 35px;
+      height: 35px;
+      display: flex;
+      justify-content: center;
+      /* 水平居中 */
+      align-items: center;
+      /* 垂直居中 */
+
+      img {
+        border-radius: 50%;
+        border-style: solid;
+        border-color: white;
+        border-width: 2px;
+        overflow: hidden;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .personInfo {
+      margin-left: 5px;
+      gap: 5px;
+      padding: 5px;
+      display: flex;
+      flex-direction: column;
+
+      span {
+        color: white;
+        font-size: small;
+      }
+    }
+  }
+
+  .chatTools {
+    display: flex;
+    gap: 10px;
+    // justify-content: center;
+    align-items: center;
+    .svgItem {
+      width: 30px;
+      height: 30px;
+      :hover {
+        color: aqua;
+      }
+    }
+  }
+}
+</style>
--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,35 +1,45 @@
 <template>
-  <div class="chatHome">
-    <el-row>
-      <el-col :span="6" class="chatBar">
-        <div class="title">
-          <h1>聊天室</h1>
+  <el-row class="chatHome">
+    <el-col :span="6" class="chatBar">
+      <div class="title">
+        <h1>聊天室</h1>
+      </div>
+      <div class="chatPersion">
+        <span class="chatPersonTitle">聊天列表</span>
+        <div class="chatPersonList">
+          <personItem class="chatPersonListItem"></personItem>
+          <personItem class="chatPersonListItem"></personItem>
+          <personItem class="chatPersonListItem"></personItem>
+          <personItem class="chatPersonListItem"></personItem>
         </div>
-        <div class="chatPersion">
-          <span class="chatPersonTitle">聊天列表</span>
-          <div class="chatPersonList">
-            <personItem class="chatPersonListItem"></personItem>
-            <personItem class="chatPersonListItem"></personItem>
-            <personItem class="chatPersonListItem"></personItem>
-            <personItem class="chatPersonListItem"></personItem>
-          </div>
-        </div>
-      </el-col>
-      <el-col :span="18"></el-col>
-    </el-row>
-  </div>
+      </div>
+    </el-col>
+    <el-col :span="18" class="chatWindowContainer">
+      <div class="chatHeader">
+        <chatWindowHeader></chatWindowHeader>
+      </div>
+      <div class="chatWindow">
+        <div class="chatWindowRecord">聊天记录</div>
+        <div class="chatWindowSendBar">发送框</div>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
 import personItem from '@/components/chat/personItem.vue'
+import chatWindowHeader from '@/components/chat/chatWindow/chatWindowHeader.vue'
 </script>
 
 <style lang="scss" scoped>
 .chatHome {
+  width: 100wh;
+  height: 100vh;
+  padding: 10px;
+
   .chatBar {
     padding: 10px;
-    width: 100wh;
-    height: 100vh;
+
     // display: flex;
 
     .title {
@@ -43,10 +53,10 @@ import personItem from '@/components/chat/personItem.vue'
     }
 
     .chatPersion {
-      height: 70%;
+      // height: 100%;
       // width:100%;
       position: relative;
-      top: 10%;
+      top: 20%;
       padding: 10px;
 
       .chatPersonTitle {
@@ -65,6 +75,20 @@ import personItem from '@/components/chat/personItem.vue'
           background-color: rgb(34, 102, 255);
         }
       }
+    }
+  }
+
+  .chatWindowContainer {
+    height: 80%;
+
+    .chatWindow {
+      margin-top: 20px;
+      margin-left: 10px;
+      margin-right: 10px;
+      height: 100%;
+      border-style: solid;
+      border-radius: 10px;
+      background-color: rgb(50, 54, 68);
     }
   }
 }
